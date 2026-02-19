0ae8ef41ad1652d451de66cc23451c31cc2c297f
Yosef shoval <ys0542522504@gmail.com>
2026-02-19 11:08:22 +0200

    write the main listener that move the atention to relevant queue

--- consumer/main.py
+++ consumer/main.py
@@ -7,47 +7,61 @@ import json
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-def add_timestemp(alert: dict):
+r = get_redis_connection()
+
+def add_timestamp(alert: dict):
     current_time = datetime.now().strftime(format="%d/%m/%Y, %H:%M:%S")
     alert["time_insertion"] = current_time
+    logger.info(f'timestamp added: {current_time}')
     return alert
 
-r = get_redis_connection()
 
 def queue_urgent_listener():
     queue_name = "queue_urgent"
     while True:
-        logger.info(f'Messages waite in queue: {r.llen(name=queue_name)}')
-        alert = r.brpoplpush(queue_name, 'tmp_queue')
-        if not alert:
-            return False
-        dict_alert = json.loads(alert)
-        add_timestemp(dict_alert)
-        save_alert(dict_alert)
-        r.lrem('tmp_queue', 1, alert)
-        return True
+        try:
+            logger.info(f'Messages waite in queue: {r.llen(name=queue_name)}')
+            alert = r.brpoplpush(queue_name, 'tmp_queue')
+            if not alert:
+                return False
+            dict_alert = json.loads(alert)
+            add_timestamp(dict_alert)
+            save_alert(dict_alert)
+            r.lrem('tmp_queue', 1, alert)
+            return True
+        except Exception as e:
+            logger.error(e)
 
 
 
 def queue_normal_listener():
     queue_name = "queue_normal"
     while True:
-        logger.info(f'Messages waite in queue: {r.llen(name=queue_name)}')
-        alert = r.brpoplpush(queue_name, 'tmp_queue')
-        if not alert:
-            return False
-        dict_alert = json.loads(alert)
-        add_timestemp(dict_alert)
-        save_alert(dict_alert)
-        r.lrem('tmp_queue', 1, alert)
-        return True
+        try:
+            logger.info(f'Messages waite in queue: {r.llen(name=queue_name)}')
+            alert = r.brpoplpush(queue_name, 'tmp_queue')
+            if not alert:
+                return False
+            dict_alert = json.loads(alert)
+            add_timestemp(dict_alert)
+            save_alert(dict_alert)
+            r.lrem('tmp_queue', 1, alert)
+        except Exception as e:
+            logger.error(e)
+    return True
 
 
 
 def main_listener():
-    pass
+    while True:
+        logger.info('start listen to urgent queue')
+        success = queue_urgent_listener()
+        if not success:
+            logger.info('urgent queue is empty')
+            logger.info('start listen to normal queue')
+            success = queue_normal_listener()
+            if not success:
+                logger.info('normal queue is empty')
+                continue
 
-
-
-
-new = "time_insertion"
+main_listener()
--- consumer/mongo_connection.py
+++ consumer/mongo_connection.py
@@ -1,5 +1,12 @@
 from pymongo import MongoClient
 import logging
+from os import getenv
+
+
+MONGODB_COLLECTION = getenv('MONGODB_COLLECTION', 'pizza_orders')
+MONGODB_DATABASE = getenv('MONGODB_DATABASE', 'pizza_orders')
+MONGO_URI = getenv('MONGO_URI', 'mongodb://root:password@localhost:27017/?authSource=admin')
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -10,7 +17,6 @@ def get_collection():
     db = client[MONGO_DATABASE]
     collection = db[MONGO_COLLECTION]
     return collection
-
 
 
 def save_alert(alert: dict):
